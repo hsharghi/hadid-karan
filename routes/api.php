126dde88d71366ce3f0a281e4dfd011ea493146e
Hadi Sharghi <hsharghi@gmail.com>
2020-02-08 17:39:54 +0330

    rename machinery route to machineries

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 
 
 Route::Resource('workers', 'WorkerController');
-Route::Resource('machinery', 'MachineryController');
+Route::Resource('machineries', 'MachineryController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
