f5bbcc13ae5b01dce51e9626fb8a975295d2f50a
Hadi Sharghi <hsharghi@gmail.com>
2020-02-08 17:54:34 +0330

    add request store rule for all models

---
+++ app/Http/Requests/JobStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Job;
+use Illuminate\Foundation\Http\FormRequest;
+
+class JobStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'customer_name' => 'required',
+            'inquiry_number' => 'nullable',
+            'type' => 'in:' . implode(',', Job::JOB_TYPES),
+            'amount' => 'nullable|numeric',
+            'quantity' => 'nullable|numeric',
+            'material' => 'nullable',
+            'weight' => 'nullable',
+        ];
+    }
+}
